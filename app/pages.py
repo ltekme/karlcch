932dc417d397592a03a1fc224f3c23429577a1fd
Karl <karl@ltek.me>
2024-07-25 21:18:01 +0000

    switched back to writing html

--- app/pages.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-from . import sitemapper, app
-
-@sitemapper.include(lastmod="2024-05-09")
-@app.route('/')
-def index():
-    return render_template('index.html.j2', title='KarlCCH | Home', metadata_description='Home of karlcch', config=app.config)
